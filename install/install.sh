e0cc9268bbbc813415a20ebe564f40329f761b1a
新运维社区 <admin@unixhot.com>
2020-04-07 19:30:27 +0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
-INSTALL_PATH="/opt/opsbt"
 
-yum install -y supervisor
+install_init(){
+    INSTALL_PATH="/opt/filebt"
+    LOG_PATH=${INSTALL_PATH}/logs
+    DATA_PATH=${INSTALL_PATH}/data
+    mkdir -p ${INSTALL_APTH} ${LOG_PATH} ${DATA_PATH}
+    /bin/cp -r ../src $INSTALL_PATH 
+    echo "------ code deploy end ------"
+}
 
-cp -r ../src $INSTALL_PATH 
-cp ./supervisord.d/*.ini /etc/supervisord.d/
+tracker(){
+    yum install -y supervisor
+    /bin/cp ./supervisord.d/*.ini /etc/supervisord.d/
+    systemctl start supervisord && systemctl enable supervisord
+    supervisorctl status
+    echo "------ filebt-tracker------"
+    netstat -ntlp | grep 8998
+}
+
+usage(){
+    echo
+    echo "Usage: $0 (install | uninstall | tracker)"
+    echo
+}
+
+
+main(){
+    case $1 in
+	install)
+		install_init;
+		;;
+        tracker)
+		install_init;
+		tracker;
+		;;
+	*)
+		usage;
+    esac
+}
+
+main $1
